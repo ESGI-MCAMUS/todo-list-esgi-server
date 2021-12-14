bfbda5efed688e2d8fa81e9a56319d2b0e0101b2
Milan Camus <pro@turtletv.fr>
2021-12-14 13:53:19 +0100

    feat: ✨ Add TS interfaces

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -12,3 +12,32 @@ export interface SQLiteQueryParams {
 export interface SQLiteSelectParams {
   query: string;
 }
+
+/* USER */
+
+export interface User {
+  id: number;
+  firstname: string;
+  lastname: string;
+  email: string;
+  password: string;
+  birthdate: string;
+}
+export interface UserLogin {
+  email: string;
+  password: string;
+}
+export interface UserRegister {
+  firstname: string;
+  lastname: string;
+  email: string;
+  password: string;
+  birthdate: string;
+}
+
+/* ITEM */
+
+export interface ItemAdd {
+  nom: string;
+  content: string;
+}
